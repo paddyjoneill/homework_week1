3690c2dadec9b9b4be8e4780ad6fa566ef8153ca
Patrick O'Neill <paddyjoneill@hotmail.com>
2019-11-22 12:13:02 +0000

    9 tests passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -21,3 +21,14 @@ end
 def stock_count(pet_shop)
   pet_shop[:pets].count
 end
+
+def pets_by_breed(pet_shop, pet_breed)
+  breed_array = []
+  pets_array = pet_shop[:pets]
+  for pet in pets_array
+    if pet[:breed] == pet_breed
+      breed_array << pet[:breed]
+    end
+  end
+  return breed_array
+end
