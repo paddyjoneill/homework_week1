5c832ff0b280cadc742e8eeda74216bbe0b467d3
Patrick O'Neill <paddyjoneill@hotmail.com>
2019-11-22 11:56:11 +0000

    third test passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,11 @@
 def pet_shop_name(pet_shop)
   pet_shop[:name]
 end
+
+def total_cash(pet_shop)
+  pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop,amount)
+  pet_shop[:admin][:total_cash] += amount
+end
